a3ee74a43426e8acdaae82e3584e87ac82aca8c5
Kyle Fischer <kfisch713@gmail.com>
2017-03-04 13:05:38 -0600

    avoiding merge conflict before dan refactors everything

--- PC_Wrapper/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ PC_Wrapper/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 
@@ -14,6 +15,7 @@ namespace ConsoleApplication2
         {
             GattReadResult result = null;
             BluetoothLEDevice device = await BluetoothLEDevice.FromBluetoothAddressAsync(000000000001);
+            Stopwatch stopwatch = new Stopwatch();
             Console.WriteLine(device);
 
             foreach (GattDeviceService i in device.GattServices)
@@ -42,16 +44,24 @@ namespace ConsoleApplication2
                         Console.WriteLine(output);
                     */
 
-                    if (i.Uuid == new Guid("03000000-0000-0000-0000-000000000000"))
+                    
+
+                    if (i.Uuid == new Guid("04000000-0000-0000-0000-000000000000"))
                     {
                         try
                         {
+                            stopwatch.Reset();
+                            stopwatch.Start();
                             result = await i.ReadValueAsync(BluetoothCacheMode.Uncached);
                             if (result.Status == GattCommunicationStatus.Success)
                             {
                                 var dataReader = Windows.Storage.Streams.DataReader.FromBuffer(result.Value);
                                 var output = dataReader.ReadString(result.Value.Length);
-                                Console.WriteLine(output);
+                                //Console.WriteLine(String.Format("raw output: {0}", output));
+                                Int16 data = (Int16) UInt16.Parse(output);
+                                Console.WriteLine(data * (6.0/32768.0));
+                                stopwatch.Stop();
+                                //Console.WriteLine(stopwatch.ElapsedMilliseconds);
                             }
                             else
                             {
@@ -63,7 +73,7 @@ namespace ConsoleApplication2
                         {
                             Console.WriteLine(e);
                         }
-                        
+
                     }
                 }
             }
