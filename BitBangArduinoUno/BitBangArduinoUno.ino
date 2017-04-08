f8fc2b645eb5b571bbb3c2f95d5b76839ed2eae7
unknown <mpl@iastate.edu>
2017-04-08 16:15:23 -0500

    Calculated accelerometer, magnetometer, and gyroscope for arduino

--- BitBangArduinoUno/BitBangArduinoUno.ino
+++ BitBangArduinoUno/BitBangArduinoUno.ino
@@ -1,38 +1,45 @@
-/*
- * LSM9DS0 Control
- * 
- * Arduino  Breakout  SPI    Datasheet  English
- * ---------------------------------------------------------
- * 9        CSAG      SS     CS         Slave Select
- * 11       SDA       MOSI   SDI        Master Out Slave In
- * 12       SDOXM     MISO   SDO        Master In Slave Out
- * 13       SCL       SCK    SPC        Clock
- * 
- */
+ /*
+  * Top to bottom
+  *        CS_AG   9
+  *        MOSI    11 
+  * 10 CSM MISO    12
+  *        SCLK    13
+  *        GND     gnd
+  *        VCC     3.3V
+  */
 
 #include "LSM9DS0.h"
 
 /* Pin outs. */
-int ss = 9;
+int ss_ag = 9;
+int ss_m = 10;
 int mosi = 11;
 int miso = 12;
 int sck = 13;
 
 /* Create an instance of the LSM9DS0. */
-LSM9DS0 lsm(ss, mosi, miso, sck);
+LSM9DS0 lsm(ss_ag, ss_m, mosi, miso, sck);
 
 void setup() {
   Serial.begin(115200);
 
   /* Start up SPI and read the status register. */
-  lsm.spi_read_byte(WHO_AM_I_XM);
-  Serial.println(lsm.spi_read_byte(WHO_AM_I_XM), HEX);
+  lsm.spi_read_byte_ag(WHO_AM_I_XM);
+  Serial.println(lsm.spi_read_byte_ag(WHO_AM_I_XM), HEX);
 
   /* Initialize accelerometer control registers. */
   lsm.init_accelerometer();
+
+  /* Initialize gyroscope control registers. */
+  lsm.init_gyroscope();
+
+  /* Initialize magnetometer control registers. */
+  lsm.init_magnetometer();
 }
 
 void loop() {
-  lsm.print_raw_accelerometer();
+  //lsm.print_calculated_accelerometer();
+  //lsm.print_calculated_gyroscope();
+  lsm.print_calculated_magnetometer();
 }
 
--- BitBangArduinoUno/LSM9DS0.h
+++ BitBangArduinoUno/LSM9DS0.h
@@ -14,28 +14,49 @@
 #define MULTIPLE_READ   0xC0
 #define WHO_AM_I_XM     0x0f
 #define FIFO_CTRL_REG   0x2E
+#define CTRL_REG1_G     0x10
+#define CTRL_REG2_G     0x11
+#define CTRL_REG3_G     0x12
+#define CTRL_REG4       0x1E
+#define ORIENT_CFG_G    0x13
 #define CTRL_REG5_XL    0x1F
 #define CTRL_REG6_XL    0x20
 #define CTRL_REG7_XL    0x21
 #define OUT_X_L_XL      0x28
+#define OUT_X_L_G       0x18
 #define FIFO_SRC_REG    0x2F
+#define CTRL_REG1_M     0x20
+#define CTRL_REG2_M     0x21
+#define CTRL_REG3_M     0x22
+#define CTRL_REG4_M     0x23
+#define CTRL_REG5_M     0x24
+#define OUT_X_L_M       0x28
 
 class LSM9DS0{
   public:
     /* Initialize an LSM9DS0 object and set the slave select pin high. */
-    LSM9DS0(int ss, int mosi, int miso, int sck);
+    LSM9DS0(int ss_ag, int ss_m, int mosi, int miso, int sck);
 
     /* Transfer a byte to the device and read a byte from the device. */
     byte spi_transfer(byte data);
 
     /* Reads a register and returns the value. */
-    byte spi_read_byte(byte address);
+    byte spi_read_byte_ag(byte address);
+
+    /* Reads a register and returns the value. */
+    byte spi_read_byte_m(byte address);
 
     /* Read a certain number of bytes from a register. */
-    void spi_read_bytes(byte address, byte* data, byte count);
+    void spi_read_bytes_ag(byte address, byte* data, byte count);
+
+    /* Read a certain number of bytes from a register. */
+    void spi_read_bytes_m(byte address, byte* data, byte count);
     
     /* Writes a value to a specified register. */
-    void spi_write(byte address, byte data);
+    void spi_write_ag(byte address, byte data);
+
+    /* Writes a value to a specified register. */
+    void spi_write_m(byte address, byte data);
 
     /* Reads accelerometer values and prints them raw. */
     void print_raw_accelerometer();
@@ -46,38 +67,30 @@ class LSM9DS0{
     /* Initialize data transfer registers for accelerometer output. */
     void init_accelerometer();
 
-    /* All possible output data rates of the accelerometer. */
-    enum a_odr {
-      A_POWER_DOWN, /* Power down mode (0000) */
-      A_ODR_3125,   /* 3.125 Hz        (0001) */
-      A_ODR_625,    /* 6.25 Hz         (0010) */
-      A_ODR_125,    /* 12.5 Hz         (0011) */
-      A_ODR_25,     /* 25 Hz           (0100) */
-      A_ODR_50,     /* 50 Hz           (0101) */
-      A_ODR_100,    /* 100 Hz          (0110) */
-      A_ODR_200,    /* 200 Hz          (0111) */
-      A_ODR_400,    /* 400 Hz          (1000) */
-      A_ODR_800,    /* 800 Hz          (1001) */
-      A_ODR_1600    /* 1600 Hz         (1010) */
-    };
+    /* Initialize data transfer registers for gyroscope output. */
+    void init_gyroscope();
 
-    /* All possible scale values for the accelerometer. */
-    enum a_scale {
-      A_SCALE_2G,  /* 2G  (000) */
-      A_SCALE_4G,  /* 4G  (001) */
-      A_SCALE_6G,  /* 6G  (010) */
-      A_SCALE_8G,  /* 8G  (011) */
-      A_SCALE_16G, /* 16G (100) */
-    };
+    /* Reads gyroscope values and prints them raw. */
+    void print_raw_gyroscope();
+
+    /* Reads and calculates gyroscope values and prints them. */
+    void print_calculated_gyroscope();
+    
+    /* Initialize data transfer registers for magnetometer output. */
+    void init_magnetometer();
+    
+    /* Reads magnetometer values and prints them raw. */
+    void print_raw_magnetometer();
+
+    /* Reads and calculates magnetometer values and prints them. */
+    void print_calculated_magnetometer();
 
   private:
-    int SS_PIN;
+    int SS_AG_PIN;
+    int SS_M_PIN;
     int MOSI_PIN;
     int MISO_PIN;
     int SCK_PIN;
-
-    a_odr accelerometer_odr;
-    a_scale accelerometer_scale;
 };
 
 #endif
